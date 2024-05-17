289078393385e1a7125f87679cbc381efaf692e4
AYUSH SHARMA <120780215+AyushSharma-007@users.noreply.github.com>
2024-05-17 09:25:42 +0530

    main.py
    This is the code of simple game of rock paper scissor in python

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
+#rockpaperscissor
+def initials():
+    player1 = input("Choose you option player-1 \n 1. R for rock \n 2. S for scissor \n 3. P for paper\n").upper()
+    player2 = input("Choose you option player-2 \n 1. R for rock \n 2. S for scissor \n 3. P for paper\n").upper()
+    return player1,player2
+def win(a,b):
+      counta = 0 
+      countb = 0
+      if a ==  b  : print('TIE')    
+      elif a =='R' and b == 'S' : 
+          print('PLAYER-1 WINS') 
+          counta += 1 
+      elif a == 'S' and b == 'P' : 
+          print('PLAYER-1 WINS') 
+          counta += 1      
+      elif a == 'P' and b == 'R' : 
+          print('PLAYER-1 WINS') 
+          counta += 1 
+      elif a == 'R' and b == 'P' : 
+          print('PLAYER-2 WINS') 
+          countb += 1       
+      elif a == 'S' and b == 'R' : 
+          print('PLAYER -2 WINS') 
+          countb += 1 
+      elif a == 'P' and b == 'S' : 
+          print('PLAYER - 2 WINS') 
+          countb += 1
+      else:
+          print("Enter the valid option")
+      return counta,countb  
+     
+def total(c,d):      
+      if c>d  : print("Player-1 WINS MORE MATCHES")
+      elif d>c: print("Player-2 WINS MORE MACTHES")
+      else    : print("ITS A TIE")
+          
 print("-------------WELCOME TO THE GAME OF ROCK PAPER SCISSOR------------")
-counta = 0
-countb = 0
-for i in  range(0,3):
-    a = input("Choose you option player-1 \n 1. R for rock \n 2. S for scissor \n 3. P for paper\n").upper()
-    b = input("Choose you option player-2 \n 1. R for rock \n 2. S for scissor \n 3. P for paper\n").upper()
-    if a == 'R' and b == 'R' : 
-              print('TIE')
-    elif a =='R' and b == 'S' : 
-              print('PLAYER-1 WINS') 
-              counta += 1 
-    elif a == 'R' and b == 'P' : 
-              print('PLAYER-2 WINS') 
-              countb += 1 
-    elif a == 'S' and b == 'R' : 
-              print('PLAYER -2 WINS') 
-              countb += 1 
-    elif a == 'S' and b == 'P' : 
-              print('player -1 wins') 
-              counta += 1 
-    elif a == 'S' and b == 'S' : 
-              print('TIE')
-    elif a == 'P' and b == 'R' : 
-              print('PLAYER-1 WINS') 
-              counta += 1 
-    elif a == 'P' and b == 'P' : 
-              print('TIE')
-    elif a == 'P' and b == 'S' : 
-              print('PLAYER - 2 WINS') 
-              countb += 1 
-    else:
-        print("Enter a valid option")
-if counta > countb:
-    print("player 1 wins the series")
-elif countb>counta:
-    print("player 2 wins the series")
-else:
-    print("Its a tie")
+for i in range(0,5):  
+     a,b = initials()
+     c,d = win(a,b)
+total(c,d)
